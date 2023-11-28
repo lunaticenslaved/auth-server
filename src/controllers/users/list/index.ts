02bf1f2ce0e2ea6b7637288ec2b5868aa7e442c8
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-11-28 23:38:30 +0300

    feat: update listing users

--- src/controllers/users/list/index.ts
+++ src/controllers/users/list/index.ts
@@ -3,6 +3,7 @@ import { createOperation } from '#/context';
 type ListUsersRequestQuery =
   | {
       userIds: string[];
+      search: string;
     }
   | {
       search: string;
@@ -13,7 +14,7 @@ export const list = createOperation(async (request, _, context) => {
   const query = request.query as unknown as ListUsersRequestQuery;
   const user =
     'userIds' in query
-      ? await context.service.user.list({ userIds: query.userIds })
+      ? await context.service.user.list({ userIds: query.userIds, search: query.search })
       : await context.service.user.list({ search: query.search, take: query.take });
 
   return { user };
