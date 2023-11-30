04b6ee1d85c14b80f36ca1bb3b71fc1e08fdad85
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-11-30 22:11:14 +0300

    fix(start): rename crypto to cript

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import * as Constants from './constants';
-import * as CryptoUtils from './crypto';
+import * as CryptoUtils from './crypt';
 import * as RequestUtils from './request';
 import * as TokensUtils from './tokens';
 
